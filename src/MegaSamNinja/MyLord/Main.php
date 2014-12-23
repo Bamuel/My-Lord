1b348833159ec369098fb5356d2ae7830c90deaf
Samuel <Samuel_ipad2@hotmail.com>
2014-12-23 12:05:54 +1100

    Update Main.php

--- src/MegaSamNinja/MyLord/Main.php
+++ src/MegaSamNinja/MyLord/Main.php
@@ -1,26 +1,43 @@
 <?php
 
-namespace megasamninja\MyLord;
+namespace megasamninjaMyLord;
 
-use pocketmine\event\Listener;
-use pocketmine\event\player\PlayerCommandPreprocessEvent;
-use pocketmine\Player;
-use pocketmine\plugin\PluginBase;
-use pocketmine\utils\TextFormat;
-
+    use pocketmineeventListener;
+    use pocketmineeventplayerPlayerCommandPreprocessEvent;
+    use pocketminePlayer;
+    use pocketminepluginPluginBase;
+    use pocketmineutilsTextFormat;
+    
 class Main extends PluginBase implements Listener{
-	
-	public function onEnable(){
-	  $this->getLogger()->info("Lord has turned on the plugin");
-    }
-	
-	public function onChatCommand(PlayerCommandPreprocessEvent $event){
-		$message = "MegaSamNinja";
-		$lord = "My Lord";
-		$player = $event->getPlayer();
-		if($message{0} != "/"){
-			$message = str_replace($lord); //Not working here
-			return true;
-		}
-	}
-}
+        
+        public function onEnable(){
+            $this->getLogger()->info("Lord has turned on the plugin");
+        }
+
+        public function onChatCommand(PlayerCommandPreprocessEvent $event){
+            $code = "MegaSamNinja";
+            $lord = "My Lord";
+            $player = $event->getPlayer();
+            if($message{0}!= "/"){
+                $tempmessage = $message;
+                $messagewords = str_word_count($message, 1);
+                for($i = 0; $i < count($messagewords); $i++){
+                        if($code["delete-message"] == true){
+                            $event->setCancelled(true);
+                        }
+                        if($code["enable-replace"] == true){
+                            $length = strlen($messagewords[$i]);
+                            $replace = "";
+                            for ($l = 0; $l < $length; $l++){
+                                $replace = $replace . "*";
+                            }
+                            $tempmessage = str_replace($messagewords[$i],$replace,$tempmessage);
+                            $replace = $code["replace-word"];
+                            $tempmessage = str_replace($messagewords[$i],$replace,$tempmessage);
+                            return true; //funny I dont know why i always do return true, Maybe its to make it happen ?
+                        }
+
+                    }
+
+                }
+        }
